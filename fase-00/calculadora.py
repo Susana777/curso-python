259d801d956883db790e2aa1242d487afa36413a
Susana777 <susananolascoalvarez97@gmail.com>
2026-08-02 23:50:10 -0600

    Agrego calculadora

---
+++ fase-00/calculadora.py
@@ -0,0 +1,30 @@
+
+def calculadora():
+    
+    while True:
+        num1 = float(input("Ingresa el primer numero: "))
+        operador = input("Ingresa el operador +, -, *, / :")
+        num2 = float(input("Ingresa el segundo numero: "))
+        
+        if operador == "+":
+            resultado = num1 + num2
+        elif operador == "-":
+            resultado = num1 - num2
+        elif operador == "*":
+            resultado = num1 * num2
+        elif operador == "/":
+            if num2 == 0:
+                print("No se puede dividir entre cero")
+            else:
+                resultado = num1 / num2
+        else:
+            print("Operador invalido")
+            
+        print(f"El resultado es: {resultado}")
+            
+        continuar = input("Quieres hacer otra operacion? (s/n): ")
+        if continuar.lower() != "s":
+            break
+
+calculadora()
+
